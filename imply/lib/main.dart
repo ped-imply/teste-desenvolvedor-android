e4e9fe551c515a1344b4583587097cd980c4fb99
Dev4LifeV <vctudp.27@gmail.com>
2021-05-25 16:24:16 -0300

    Editados icones

--- imply/lib/main.dart
+++ imply/lib/main.dart
@@ -9,10 +9,78 @@ main() => runApp(GetMaterialApp(
 
 class Principal extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Container(),
-      ),
+    return Container(
+      child: Container(
+          child: DefaultTabController(
+              length: 2,
+              child: Scaffold(
+                  bottomSheet: Container(
+                    height: 80,
+                    decoration: BoxDecoration(color: Color(0xff005c92)),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                  width: 2, color: Color(0xff426fa5)),
+                              borderRadius: BorderRadius.circular(4)),
+                          width: 180,
+                          height: 50,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                child: Text("Texto aqui"),
+                              ),
+                              VerticalDivider(
+                                color: Colors.grey[600],
+                                width: 10,
+                              ),
+                              Container(
+                                child: Text("Texto aqui"),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 15),
+                          child: ElevatedButton(
+                              onPressed: () {}, child: Text("Limpar")),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: ElevatedButton(
+                              onPressed: () {}, child: Text("Confirmar")),
+                        )
+                      ],
+                    ),
+                  ),
+                  appBar: AppBar(
+                    automaticallyImplyLeading: false,
+                    backgroundColor: Colors.yellowAccent[700],
+                    centerTitle: true,
+                    title: Image.asset("assets/i-imply.png",
+                        width: 130, height: 130),
+                    bottom: TabBar(indicatorColor: Color(0xff005c92), tabs: [
+                      Tab(
+                        child: Text(
+                          "Bebidas",
+                          style: TextStyle(color: Color(0xff005c92)),
+                        ),
+                      ),
+                      Tab(
+                        child: Text(
+                          "Lanches",
+                          style: TextStyle(color: Color(0xff005c92)),
+                        ),
+                      ),
+                    ]),
+                  ),
+                  body: TabBarView(
+                    children: [Container(), Container()],
+                  )))),
     );
   }
 }
---
+++ nextar/nextar/README.md
@@ -0,0 +1,30 @@
+# Nextar System
+
+Catálogo de produtos multiusuário para gerenciamento de estoque, preços e mapeamento dos produtos.
+
+# Versão
+alpha 1.0.0+1
+
+## Instruções para manuseio do código
+
+* É necessário a versão 2.0.6 Stable Channel do Flutter para manipulação correta do código
+
+* É necessário a Dart SDK na versão >= 2.7.0 ou < 3.0.0
+
+* As dependências utilizadas para desenvolvimento do projeto são as seguintes:
+-  sqflite: ^2.0.0+3
+-  shared_preferences: ^2.0.5
+-  google_fonts: ^2.0.0
+-  get: ^4.1.4
+-  path_provider: ^2.0.1
+-  path: ^1.8.0
+-  extended_masked_text: ^2.1.0
+-  dbcrypt: ^2.0.0
+-  image_picker: ^0.7.4
+
+* Não é necessário quaisquer dependências de desenvolvedor
+
+* O sistema é suportado apenas nas plataformas: Android e iOS
+
+Quaisquer dúvidas, entre em contato por victorcristovaoproppe@gmail.com
+
