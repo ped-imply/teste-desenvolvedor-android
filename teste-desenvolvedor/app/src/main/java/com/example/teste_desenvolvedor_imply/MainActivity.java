48be538ad0399af6dcfa0d044eb862b5231ad807
DionathanSehnem <dionathan2002@gmail.com>
2023-03-12 19:48:44 -0300

    Configurado a troca de opções

--- teste-desenvolvedor/app/src/main/java/com/example/teste_desenvolvedor_imply/MainActivity.java
+++ teste-desenvolvedor/app/src/main/java/com/example/teste_desenvolvedor_imply/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.teste_desenvolvedor_imply;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLanches;
+    private Button buttonBebidas;
+    private View activeLanches;
+    private View activeBebidas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonBebidas = findViewById(R.id.buttonBebidas);
+        buttonLanches = findViewById(R.id.buttonLanches);
+
+        activeBebidas = findViewById(R.id.activeBebidas);
+        activeLanches = findViewById(R.id.activeLanches);
+
+        buttonLanches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Lanches");
+                activeBebidas.setVisibility(View.INVISIBLE);
+                activeLanches.setVisibility(View.VISIBLE);
+            }
+        });
+
+        buttonBebidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Bebidas");
+                activeLanches.setVisibility(View.INVISIBLE);
+                activeBebidas.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
